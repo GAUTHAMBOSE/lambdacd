6c7c99faad4fbd893869ededd5e44be571c14cdc
GAUTHAMBOSE <gauthambosec.com>
2023-08-28 14:15:31 +0530

    yu lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,11 @@
-def lambda_handler(event, context):
-    print("Hello World!")
-    print("This is a test")
+import pandas as pd
 
+def lambda_handler(event, context):
+    d = {'col1': [1, 2], 'col2': [3, 4]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    print("Done x1")
+    return df.to_json()
 
 
 
--- post_traffic_hook.py
+++ post_traffic_hook.py
@@ -1,6 +1,6 @@
-import boto3
+# import boto3
 
-def lambda_handler(event, context):
-    print("version 2.0")
-    print("working on it")
-    pass
+# def lambda_handler(event, context):
+#     print("version 2.0")
+#     print("working on it")
+#     pass
